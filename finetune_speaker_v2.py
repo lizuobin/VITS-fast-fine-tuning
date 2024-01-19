cf9752b18031f60f9fb6767823ee98b868e650de
lizuobin <lizuobin@baidu.com>
2024-01-19 15:00:32 +0800

    update

--- finetune_speaker_v2.py
+++ finetune_speaker_v2.py
@@ -171,6 +171,8 @@ def train_and_evaluate(rank, epoch, hps, nets, optims, schedulers, scaler, loade
 
   net_g.train()
   net_d.train()
+  print("train_loader:")
+  print(len(train_loader))
   for batch_idx, (x, x_lengths, spec, spec_lengths, y, y_lengths, speakers) in enumerate(tqdm(train_loader)):
     x, x_lengths = x.cuda(rank, non_blocking=True), x_lengths.cuda(rank, non_blocking=True)
     spec, spec_lengths = spec.cuda(rank, non_blocking=True), spec_lengths.cuda(rank, non_blocking=True)
